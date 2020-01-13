4fa3e81cd37be1e9c07e0e162c80882063154e6f
Dor Azriel <doraz4.ac.sce.ac.il>
2020-01-13 23:05:15 +0200

    Add project files.

---
+++ read/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace read.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
