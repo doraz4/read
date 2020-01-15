47dde3ee5660d42cfd35d77ba57f15011fdc596a
dor <dorro1@ac.sce.ac.il>
2020-01-15 12:21:12 +0200

    fix home page :)

---
+++ read/Data/MvcBookContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DataBookClass> Book { get; set; }
+        // check if DataBookClass is ok 
+    }
+}
---
+++ read/Models/DataBookClass.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class DataBookClass
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
