e9bd133e062dab1997e1375c774d4b34396bc2c3
noamBar <noamba2@ac.sce.ac.il>
2020-01-16 14:49:36 +0200

    changes nav bar

--- read/Controllers/HomeController.cs
+++ read/Controllers/HomeController.cs
@@ -17,39 +17,21 @@ namespace read.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            
             return View();
         }
-
+       
         public IActionResult Book()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
-          
-            ViewData["Message"] = "Your contact page.";
+         
 
             return View();
           
-        }
-
-        public IActionResult GetStart()
-        {
-
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
