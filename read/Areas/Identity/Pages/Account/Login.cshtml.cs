e4151cfd254ee564fd43dafae502bcb336688fdd
Dor Azriel <doraz4.ac.sce.ac.il>
2020-01-17 20:13:25 +0200

    Authoriztion

--- read/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ read/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using read.Models;
-
+//
 namespace read.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
